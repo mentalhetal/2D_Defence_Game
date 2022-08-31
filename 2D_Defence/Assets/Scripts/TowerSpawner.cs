70831e7444fdd973113e955a08bf1a6a50b2d0fd
mentalhetal <37432702+mentalhetal@users.noreply.github.com>
2022-08-31 18:48:37 +0900

    타워 배치 및 적 히트 전단계
    타워 배치 및 적 히트 전단계

---
+++ 2D_Defence/Assets/Scripts/TowerSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject towerPrefab;
+
+    public void SpawnTower(Transform tileTransform)
+    {
+        Instantiate(towerPrefab, tileTransform.position, Quaternion.identity);
+    }
+}
